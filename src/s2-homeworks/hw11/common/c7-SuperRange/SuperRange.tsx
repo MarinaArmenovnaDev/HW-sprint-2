7a105ba903d248320eb94b1e9df61d156551e3be
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-08-07 00:43:01 +0300

    first commit

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,13 +1,48 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import { Slider, SliderProps } from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+            sx={{
+                width: 147,
+                height: 4,
+                color: '#00CC22',
+                padding: '13px 0',
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B',
+                    opacity: 1,
+                    height: 4,
+                    borderRadius: 10,
+                },
+                '& .MuiSlider-track': {
+                    backgroundColor: '#00CC22',
+                    height: 4,
+                    borderRadius: 10,
+                    border: 'none',
+                },
+                '& .MuiSlider-thumb': {
+                    width: 18,
+                    height: 18,
+                    backgroundColor: '#00CC22',
+                    border: '4px solid white',
+                    boxShadow: '0 0 0 2px rgba(0, 204, 34, 0.3)',
+                    '&:hover, &.Mui-active': {
+                        boxShadow: '0 0 0 4px rgba(0, 204, 34, 0.3)',
+                    },
+                    '&:before': {
+                        display: 'none',
+                    },
+                },
+                '& .MuiSlider-valueLabel': {
+                    backgroundColor: 'transparent',
+                    color: '#000',
+                    fontWeight: 'bold',
+                    fontSize: 12,
+                    top: -30,
+                },
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            {...props}
         />
     )
 }
