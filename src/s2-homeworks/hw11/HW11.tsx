ea09afc2e7da8b33bc3f0662f49e1454a1888a84
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-08-07 01:16:44 +0300

    first commit

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -16,12 +16,12 @@ function HW11() {
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
 
-    const change = (event: Event, value: number | number[]) => {
+    const change = (event: React.SyntheticEvent | Event, value: number | number[]) => {
         if(Array.isArray(value)) {
             setValue1(value[0])
             setValue2(value[1])
         }else{
-            setValue1(value)
+            setValue1(value as number)
         }
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
