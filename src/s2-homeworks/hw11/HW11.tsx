60e9a24b9220a5e606937d8550e836fd263621bc
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-08-07 16:32:43 +0300

    first commit

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -5,26 +5,25 @@ import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 function HW11() {
-
-
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
-
-
 
     const change = (event: Event, value: number | number[]) => {
         if (Array.isArray(value)) {
             setValue1(value[0])
             setValue2(value[1])
         } else {
-            setValue1(value as number)
+            setValue1(value)
+            // Ensure value2 is always greater than value1
+            if (value >= value2) {
+                setValue2(Math.min(100, value + 1))
+            }
         }
     }
 
     return (
         <div id={'hw11'}>
             <div className={s2.hwTitle}>Homework #11</div>
-
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
@@ -33,6 +32,7 @@ function HW11() {
                             id={'hw11-single-slider'}
                             value={value1}
                             onChange={change}
+                            data-testid="single-slider"
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -41,6 +41,7 @@ function HW11() {
                             id={'hw11-double-slider'}
                             value={[value1, value2]}
                             onChange={change}
+                            data-testid="double-slider"
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,28 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{
-
+                width: '300px',
+                margin: '0 10px',
+                color: '#00CC22',
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B',
+                    opacity: 1,
+                    height: 4,
+                },
+                '& .MuiSlider-track': {
+                    backgroundColor: '#00CC22',
+                    height: 4,
+                    border: 'none',
+                },
+                '& .MuiSlider-thumb': {
+                    width: 18,
+                    height: 18,
+                    backgroundColor: '#00CC22',
+                    border: '4px solid white',
+                    '&:focus, &:hover, &.Mui-active': {
+                        boxShadow: '0 0 0 4px rgba(0, 204, 34, 0.3)',
+                    },
+                },
             }}
             {...props}
         />
